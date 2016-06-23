12bcbd7d07a7ff756b9c349a83bfa407326da3d9
fauzanhilmi <fauzanhilmir@gmail.com>
2016-06-23 11:06:52 +0700

    merging class FieldTable with Field

--- FiniteField/Program.cs
+++ FiniteField/Program.cs
@@ -6,47 +6,52 @@ using System.Threading.Tasks;
 
 namespace GaloisField
 {
-    class FieldTable
+    class Field
     {
-        const byte polynomial = 0x1D; //from 0x11D
-        byte[] Exp;
-        byte[] Log;
+        //Irreducible polynomial used : x^8 + x^4 + x^3 + x^2 + 1 (0x11D)
+        public const byte polynomial = 0x1D; //0x11D & 0xFF
+        public static byte[] Exp;
+        public static byte[] Log;
 
-        public byte multiply (byte a, byte b) //using Russian Peasant Multiplication algorithm
+        private byte number;
+
+        static Field()
+        {
+
+        }
+
+        public Field()
+        {
+            number = 0; 
+        }
+
+        public Field(byte _number)
+        {
+            number = _number;
+        }
+
+        private byte multiply(byte a, byte b) 
+        //used in exp & log table generation
+        //implemented with Russian Peasant Multiplication algorithm
         {
             byte result = 0;
             byte aa = a;
             byte bb = b;
-            while(bb != 0)
+            while (bb != 0)
             {
                 if ((bb & 1) != 0)
                 {
                     result ^= aa;
                 }
-                byte highest_bit = (byte) (aa & 0x80);
+                byte highest_bit = (byte)(aa & 0x80);
                 aa <<= 1;
-                if(highest_bit != 0)
+                if (highest_bit != 0)
                 {
                     aa ^= polynomial;
                 }
                 bb >>= 1;
             }
             return result;
-        }
-    }
-
-    class Field
-    {
-        private byte number;
-
-        Field()
-        {
-            number = 0; 
-        }
-
-        Field(byte _number)
-        {
-            number = _number;
         }
 
     }
@@ -55,8 +60,8 @@ namespace GaloisField
     {
         static void Main(string[] args)
         {
-            FieldTable ft = new FieldTable();
-            Console.WriteLine(ft.multiply(255,239));
+            Field f = new Field();
+            //Console.WriteLine(f.multiply(255,239));
             Console.ReadLine();
         }
     }
